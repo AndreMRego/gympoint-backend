9f73005f9658d30df65982a83a3cb02b526f8b56
Andre Work <andremregodev@gmail.com>
2019-12-17 09:59:00 -0300

    Criando middleware de autenticacao, authConfig com bcrypt e jwt

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+/**
+ * This function is responsible for verify token and save id
+ */
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b8de876415e0826f56aa5d28fa752f74',
+  expiresIn: '7d',
+};
